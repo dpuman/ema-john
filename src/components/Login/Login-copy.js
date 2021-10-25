7b414e00c7678ebecf7cf91b1d752acc91a8f072
dpuman <dpumail.in@gmail.com>
2021-10-26 01:12:11 +0600

    'done'

--- src/components/Login/Login-copy.js
+++ src/components/Login/Login-copy.js
@@ -90,13 +90,9 @@ function Login() {
                 // ...
             })
             .catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
+
                 const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = FacebookAuthProvider.credentialFromError(error);
+                console.log(errorMessage);
 
                 // ...
             });
@@ -226,6 +222,7 @@ function Login() {
             }
 
             <h1>Signup form</h1>
+            <br />
 
 
             <form onSubmit={handleSubmit}>
